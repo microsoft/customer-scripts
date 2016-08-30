0c525af944b7e6311861e735a2342d5d6ba389c2
Vishal Meghani <vimegh@microsoft.com>
2016-08-30 14:50:24 +0530

    modifying Comments in the script begining

--- test/RDPSession/disconnect.bat
+++ test/RDPSession/disconnect.bat
@@ -1,4 +1,4 @@
-REM This batch script disconnects the Remote Desktop Session without locking the machine. 
+REM This batch script disconnects the Remote Desktop Session without locking the machine. Run with Administrator privileges.
 REM Useful in case of running Coded UI Tests where user want to end the remote desktop session but at the same time doesn't want to lock the machine
 
 for /F "usebackq tokens=1" %%f in (`query session ^| findstr /C:^^^>`) do set session="%%f"
