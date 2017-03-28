9686caed26e67a286cc772d20bb52532a16416dd
NikolayKa <nikolaykash@gmail.com>
2017-03-28 14:26:47 -0700

    changed to work with PS5

--- test/build/orderedtest/throwif-trxfromtestrun-ofbuild-hasfailedoutcome.ps1
+++ test/build/orderedtest/throwif-trxfromtestrun-ofbuild-hasfailedoutcome.ps1
@@ -25,15 +25,17 @@ Write-Host $testrunid
 # http://localhost:8080/tfs/DefaultCollection/testproject/_apis/test/runs/33/attachments
 Write-Host $tfsurl$teamproject/_apis/test/runs/$testrunid/attachments
 $attachments=Invoke-RestMethod -Uri $tfsurl$teamproject/_apis/test/runs/$testrunid/attachments -Method Get  -Headers $headers
-$trxattachments = $attachments | where { $_.value.filename.endswith(".trx") } 
+$trxattachments = ($attachments.value |  Where-Object { 
+  $_.attachmentType -eq "tmiTestRunSummary" 
+})
 
 if($trxattachments -eq $null -or $trxattachments.count -eq 0)
 { 
   throw "no test run attachments found of type trx"
 }
 
-$trxattachments= $trxattachments | Sort-Object { $_.value.createddate }
-$trxattachmentid=$trxattachments[0].value.id
+$trxattachments= $trxattachments.value | Sort-Object { $_.createddate }
+$trxattachmentid=$trxattachments[0].id
 
 Write-Host $trxattachmentid
 
